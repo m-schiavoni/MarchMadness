0921fd241abe24d5940dc45ae68c8fc3e7a2441d
Michael Schiavoni <62128374+m-schiavoni@users.noreply.github.com>
2020-09-06 14:34:36 -0700

    Add files via upload
    Two more SQL tables added to ingest tournament results

--- mm_web_scraper.py
+++ mm_web_scraper.py
@@ -1,12 +1,12 @@
-# This script automatically extracts a wide selection of statistics from www.sports-reference.com
-# for teams competing in the NCAA men's basketball tournament since 2010, and saves the information
-# in a six-table SQLite relational database.
+# This script automatically extracts a wide selection of statistics and tournament results
+# from www.sports-reference.com for teams competing in the NCAA men's basketball tournament
+# since 2010, and saves the information in an eight-table SQLite relational database.
 
 import os
 import re
 import time
 import sqlite3
-import urllib.request, urllib.parse, urllib.error
+import urllib.request
 
 os.chdir('C:/Users/Michael/Dropbox/Documents/Python/march madness')
 conn = sqlite3.connect('mm_web_dump.sqlite')
@@ -18,6 +18,8 @@ DROP TABLE IF EXISTS Team_Stats1;
 DROP TABLE IF EXISTS Team_Stats2;
 DROP TABLE IF EXISTS Opp_Stats1;
 DROP TABLE IF EXISTS Opp_Stats2;
+DROP TABLE IF EXISTS Rank;
+DROP TABLE IF EXISTS Tournament;
 
 CREATE TABLE School (
     id    INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT UNIQUE,
@@ -77,6 +79,22 @@ CREATE TABLE Opp_Stats2 (
     opp_blk_pct  FLOAT,
     opp_tov_pct  FLOAT,
     PRIMARY KEY (school_id, season_id)
+);
+
+CREATE TABLE Rank (
+    school_id    INTEGER,
+    season_id    INTEGER,
+    tourn_rank   INTEGER,
+    PRIMARY KEY (school_id, season_id)
+);
+
+CREATE TABLE Tournament (
+    season_id    INTEGER,
+    school_id1   INTEGER,
+    school_id2   INTEGER,
+    score1       INTEGER,
+    score2       INTEGER,
+    PRIMARY KEY (season_id, school_id1, school_id2)
 )
 ''')
 
@@ -91,39 +109,40 @@ school_dict = {'Ole Miss':'Mississippi', 'USC':'Southern California', 'Californi
                'UMBC':'Maryland-Baltimore County', 'LIU':'Long Island University', "St. Peter's":"Saint Peter's", 'Detroit':'Detroit Mercy',
                'Southern Miss':'Southern Mississippi', 'UMass':'Massachusetts', 'SMU':'Southern Methodist', 'TCU':'Texas Christian'}
 
-end_year   = int(input('Starting year = 2010. Please enter ending year...'))
-
-for year in list(range(2010, end_year+1)):
+# hard-coded to begin in 2010, because limited statistics are available for earlier seasons
+end_year = int(input('Starting year = 2010.  Please enter ending year...'))
+years = list(range(2010, end_year+1));  years.reverse()
+for year in years:
     ###### CYCLE THROUGH SEASONS ######
     print(year)
+    if (year==2020):
+        print('...no tournament (COVID-19)')
+        continue
     cur.execute('INSERT OR IGNORE INTO Season (year) VALUES ( ? )', ( year, ) )
     cur.execute('SELECT id FROM Season WHERE year = ? ', (year, ))
     season_id = cur.fetchone()[0]
 
     ###### BASIC TEAM STATISTICS ######
     print('...basic team stats')
-    time.sleep(1)
+    # time.sleep(1)
     fhand = urllib.request.urlopen('https://www.sports-reference.com/cbb/seasons/' + str(year) + '-school-stats.html')
     for line in fhand:
         linetext = line.decode().strip()
         if '<small>NCAA</small>' not in linetext:  # skip schools that didn't appear in the tournament
             continue
-        school = re.findall('data-stat="school_name" ><a href=\S*>(.*)</a>', linetext)[0]
+        school = re.findall('data-stat="school_name" ><a href=.+?>(.+?)</a>', linetext)[0]
         if ('&amp;' in school):  school = school.replace('&amp;', '&')
-        minutes = int(  re.findall('data-stat="mp" >(\S*)</td>', linetext)[0])
-        srs     = float(re.findall('data-stat="srs" >(\S*)</td>', linetext)[0])
-        pts     = int(  re.findall('data-stat="pts" >(\S*)</td>', linetext)[0])
-        fg3     = int(  re.findall('data-stat="fg3" >(\S*)</td>', linetext)[0])
-        ft      = float(re.findall('data-stat="ft" >(\S*)</td>', linetext)[0])
-        # try:                  # offensive rebounds are not recorded for some schools in earlier seasons
-        off_reb = int(  re.findall('data-stat="orb" >(\S*)</td>', linetext)[0])
-        # except:               # will need to impute missing values prior to predictive modeling
-            # off_reb = 0
-        def_reb = int(  re.findall('data-stat="trb" >(\S*)</td>', linetext)[0]) - off_reb
-        ast     = int(  re.findall('data-stat="ast" >(\S*)</td>', linetext)[0])
-        stl     = int(  re.findall('data-stat="stl" >(\S*)</td>', linetext)[0])
-        blk     = int(  re.findall('data-stat="blk" >(\S*)</td>', linetext)[0])
-        tov     = int(  re.findall('data-stat="tov" >(\S*)</td>', linetext)[0])
+        minutes = int(  re.findall('data-stat="mp" >(.+?)</td>', linetext)[0])              # minutes played
+        srs     = float(re.findall('data-stat="srs" >(.+?)</td>', linetext)[0])             # Simple Rating System
+        pts     = int(  re.findall('data-stat="pts" >(.+?)</td>', linetext)[0])             # points scored
+        fg3     = int(  re.findall('data-stat="fg3" >(.+?)</td>', linetext)[0])             # 3-pt field goals
+        ft      = float(re.findall('data-stat="ft" >(.+?)</td>', linetext)[0])              # freethrows made
+        off_reb = int(  re.findall('data-stat="orb" >(.+?)</td>', linetext)[0])             # offensive rebounds
+        def_reb = int(  re.findall('data-stat="trb" >(.+?)</td>', linetext)[0]) - off_reb   # defensive rebounds
+        ast     = int(  re.findall('data-stat="ast" >(.+?)</td>', linetext)[0])             # assists
+        stl     = int(  re.findall('data-stat="stl" >(.+?)</td>', linetext)[0])             # steals
+        blk     = int(  re.findall('data-stat="blk" >(.+?)</td>', linetext)[0])             # blocks
+        tov     = int(  re.findall('data-stat="tov" >(.+?)</td>', linetext)[0])             # turnovers
         
         cur.execute('INSERT OR IGNORE INTO School (name) VALUES ( ? )', ( school, ) )
         cur.execute('SELECT id FROM School WHERE name = ? ', (school, ))
@@ -136,23 +155,23 @@ for year in list(range(2010, end_year+1)):
 
     ###### BASIC OPPONENT STATISTICS ######
     print('...basic opponent stats')
-    time.sleep(1)
+    # time.sleep(1)
     fhand = urllib.request.urlopen('https://www.sports-reference.com/cbb/seasons/' + str(year) + '-opponent-stats.html')
     for line in fhand:
         linetext = line.decode().strip()
         if '<small>NCAA</small>' not in linetext:  # skip schools that didn't appear in the tournament
             continue
-        school = re.findall('data-stat="school_name" ><a href=\S*>(.*)</a>', linetext)[0]
+        school = re.findall('data-stat="school_name" ><a href=.+?>(.+?)</a>', linetext)[0]
         if ('&amp;' in school):  school = school.replace('&amp;', '&')
-        opp_pts     = int(  re.findall('data-stat="opp_pts" >(\S*)</td>', linetext)[0])
-        opp_fg3     = int(  re.findall('data-stat="opp_fg3" >(\S*)</td>', linetext)[0])
-        opp_fta     = float(re.findall('data-stat="opp_fta" >(\S*)</td>', linetext)[0])
-        opp_off_reb = int(  re.findall('data-stat="opp_orb" >(\S*)</td>', linetext)[0])
-        opp_def_reb = int(  re.findall('data-stat="opp_trb" >(\S*)</td>', linetext)[0]) - opp_off_reb
-        opp_ast     = int(  re.findall('data-stat="opp_ast" >(\S*)</td>', linetext)[0])
-        opp_stl     = int(  re.findall('data-stat="opp_stl" >(\S*)</td>', linetext)[0])
-        opp_blk     = int(  re.findall('data-stat="opp_blk" >(\S*)</td>', linetext)[0])
-        opp_tov     = int(  re.findall('data-stat="opp_tov" >(\S*)</td>', linetext)[0])
+        opp_pts     = int(  re.findall('data-stat="opp_pts" >(.+?)</td>', linetext)[0])                 # opponent points scored
+        opp_fg3     = int(  re.findall('data-stat="opp_fg3" >(.+?)</td>', linetext)[0])                 # opponent 3-pt field goals
+        opp_fta     = float(re.findall('data-stat="opp_fta" >(.+?)</td>', linetext)[0])                 # opponent freethrow attempts
+        opp_off_reb = int(  re.findall('data-stat="opp_orb" >(.+?)</td>', linetext)[0])                 # opponent offensive rebounds
+        opp_def_reb = int(  re.findall('data-stat="opp_trb" >(.+?)</td>', linetext)[0]) - opp_off_reb   # opponent defensive rebounds
+        opp_ast     = int(  re.findall('data-stat="opp_ast" >(.+?)</td>', linetext)[0])                 # opponent assists
+        opp_stl     = int(  re.findall('data-stat="opp_stl" >(.+?)</td>', linetext)[0])                 # opponent steals
+        opp_blk     = int(  re.findall('data-stat="opp_blk" >(.+?)</td>', linetext)[0])                 # opponent blocks
+        opp_tov     = int(  re.findall('data-stat="opp_tov" >(.+?)</td>', linetext)[0])                 # opponent turnovers
         
         # cur.execute('INSERT OR IGNORE INTO School (name) VALUES ( ? )', ( school, ) )
         cur.execute('SELECT id FROM School WHERE name = ? ', (school, ))
@@ -165,18 +184,18 @@ for year in list(range(2010, end_year+1)):
 
     ###### ADVANCED TEAM STATISTICS ######
     print('...advanced team stats')
-    time.sleep(1)
+    # time.sleep(1)
     fhand = urllib.request.urlopen('https://www.sports-reference.com/cbb/seasons/' + str(year) + '-advanced-school-stats.html')
     for line in fhand:
         linetext = line.decode().strip()
         if '<small>NCAA</small>' not in linetext:  # skip schools that didn't appear in the tournament
             continue
-        school = re.findall('data-stat="school_name" ><a href=\S*>(.*)</a>', linetext)[0]
+        school = re.findall('data-stat="school_name" ><a href=.+?>(.+?)</a>', linetext)[0]
         if ('&amp;' in school):  school = school.replace('&amp;', '&')
-        pace    = float(re.findall('data-stat="pace" >(\S*)</td>', linetext)[0])
-        ast_pct = float(re.findall('data-stat="ast_pct" >(\S*)</td>', linetext)[0])
-        blk_pct = float(re.findall('data-stat="blk_pct" >(\S*)</td>', linetext)[0])
-        tov_pct = float(re.findall('data-stat="tov_pct" >(\S*)</td>', linetext)[0])
+        pace    = float(re.findall('data-stat="pace" >(.+?)</td>', linetext)[0])        # possessions per 40 minutes
+        ast_pct = float(re.findall('data-stat="ast_pct" >(.+?)</td>', linetext)[0])     # percentage of field goals with an assist
+        blk_pct = float(re.findall('data-stat="blk_pct" >(.+?)</td>', linetext)[0])     # percentage of opponent 2-pt fg attempts blocked
+        tov_pct = float(re.findall('data-stat="tov_pct" >(.+?)</td>', linetext)[0])     # percentage of plays resulting in a turnover
 
         # cur.execute('INSERT OR IGNORE INTO School (name) VALUES ( ? )', ( school, ) )
         cur.execute('SELECT id FROM School WHERE name = ? ', (school, ))
@@ -189,16 +208,16 @@ for year in list(range(2010, end_year+1)):
 
     ###### ADVANCED OPPONENT STATISTICS ######
     print('...advanced opponent stats')
-    time.sleep(1)
+    # time.sleep(1)
     fhand = urllib.request.urlopen('https://www.sports-reference.com/cbb/seasons/' + str(year) + '-advanced-opponent-stats.html')
     for line in fhand:
         linetext = line.decode().strip()
         if '<small>NCAA</small>' not in linetext:  # skip schools that didn't appear in the tournament
             continue
-        school = re.findall('data-stat="school_name" ><a href=\S*>(.*)</a>', linetext)[0]
+        school = re.findall('data-stat="school_name" ><a href=.+?>(.+?)</a>', linetext)[0]
         if ('&amp;' in school):  school = school.replace('&amp;', '&')
-        opp_blk_pct = float(re.findall('data-stat="opp_blk_pct" >(\S*)</td>', linetext)[0])
-        opp_tov_pct = float(re.findall('data-stat="opp_tov_pct" >(\S*)</td>', linetext)[0])
+        opp_blk_pct = float(re.findall('data-stat="opp_blk_pct" >(.+?)</td>', linetext)[0])     # percentage of 2-pt fg attempts blocked by opponent
+        opp_tov_pct = float(re.findall('data-stat="opp_tov_pct" >(.+?)</td>', linetext)[0])     # percentage opponent plays resulting in a turnover
 
         # cur.execute('INSERT OR IGNORE INTO School (name) VALUES ( ? )', ( school, ) )
         cur.execute('SELECT id FROM School WHERE name = ? ', (school, ))
@@ -208,5 +227,117 @@ for year in list(range(2010, end_year+1)):
          VALUES ( ?, ?, ?, ? )''',
         (school_id, season_id, opp_blk_pct, opp_tov_pct) )
     conn.commit()
+
+    ###### TOURNAMENT RESULTS ######
+    print('...tournament results')
+    # time.sleep(1)
+    fhand = urllib.request.urlopen('https://www.sports-reference.com/cbb/postseason/' + str(year) + '-ncaa.html')
+    
+    count = 0
+    if (year==2010):  # this year only had one play-in game, whereas subsequent years have four, a.k.a. the "First Four"
+        cur.execute('SELECT id FROM School WHERE name = ? ', ('Winthrop', ))
+        school_id1 = cur.fetchone()[0]
+        cur.execute('INSERT OR IGNORE INTO Rank (season_id, school_id, tourn_rank) VALUES ( ?, ?, ? )', (season_id, school_id1, 16) )
+        cur.execute('SELECT id FROM School WHERE name = ? ', ('Arkansas-Pine Bluff', ))
+        school_id2 = cur.fetchone()[0]
+        cur.execute('INSERT OR IGNORE INTO Rank (season_id, school_id, tourn_rank) VALUES ( ?, ?, ? )', (season_id, school_id2, 16) )
+        cur.execute('INSERT OR REPLACE INTO Tournament (season_id, school_id1, school_id2, score1, score2) VALUES ( ?, ?, ?, ?, ? )',
+                    (season_id, school_id1, school_id2, 44, 61) )
+        count = count + 2
+    
+    # the toggle switch logic ensures three specific lines (rank, school, score) are found in precisely the correct order
+    toggle0 = True;  toggle1 = False;  toggle2 = False;  toggle3 = False
+    first4 = False;  pair = False
+    for line in fhand:
+        linetext = line.decode().strip()
+        if toggle0:
+            if '>Final Four</a>' in linetext:  # skip down to this line to start searching for relevant information
+                toggle0 = False;  toggle1 = True
+                continue
+        else:
+            if 'First Four</strong>' in linetext:
+                first4 = True
+            elif '<div id="bracket" class="team16">' in linetext:
+                first4 = False
+        if first4:
+            rank_list = re.findall('<strong>([0-9]+)</strong>', linetext)
+            if (len(rank_list)==2):
+                rank = rank_list[0]
+                school_list = re.findall('/cbb/schools/.+?>(.+?)</a>', linetext)
+                if ('&amp;' in school_list[0]):  school_list[0] = school_list[0].replace('&amp;', '&')
+                if ('&amp;' in school_list[1]):  school_list[1] = school_list[1].replace('&amp;', '&')
+                if (school_list[0] in school_dict):  school_list[0] = school_dict[school_list[0]]  # rename schools to match stats tables
+                if (school_list[1] in school_dict):  school_list[1] = school_dict[school_list[1]]  # rename schools to match stats tables
+                
+                cur.execute('SELECT id FROM School WHERE name = ? ', (school_list[0], ))
+                try:
+                    school_id1 = cur.fetchone()[0]
+                except:
+                    print('(!) WARNING:' + school_list[0] + ' must be added to school_dict')
+                if (school_id1==None):  print('(!) WARNING:' + school_list[0] + ' must be added to school_dict')
+
+                cur.execute('SELECT id FROM School WHERE name = ? ', (school_list[1], ))
+                try:
+                    school_id2 = cur.fetchone()[0]
+                except:
+                    print('(!) WARNING:' + school_list[1] + ' must be added to school_dict')
+                if (school_id2==None):  print('(!) WARNING:' + school_list[1] + ' must be added to school_dict')
+
+                cur.execute('INSERT OR IGNORE INTO Rank (season_id, school_id, tourn_rank) VALUES ( ?, ?, ? )', (season_id, school_id1, rank) )
+                cur.execute('INSERT OR IGNORE INTO Rank (season_id, school_id, tourn_rank) VALUES ( ?, ?, ? )', (season_id, school_id2, rank) )
+                score_list = re.findall('html.>([0-9]+)</a>', linetext)
+                cur.execute('INSERT OR REPLACE INTO Tournament (season_id, school_id1, school_id2, score1, score2) VALUES ( ?, ?, ?, ?, ? )',
+                            (season_id, school_id1, school_id2, score_list[0], score_list[1]) )
+                count = count + 2
+        elif toggle1:
+            rank = re.findall('<span>([0-9]+)</span>', linetext)
+            if len(rank)==1:
+                toggle1 = False;  toggle2 = True
+                rank = int(rank[0])
+        elif toggle2:
+            school_list = re.findall('/cbb/schools/.+?>(.+?)</a>', linetext)
+            if len(school_list)==1:
+                toggle2 = False;  toggle3 = True
+                school = school_list[0]
+            else:
+                toggle2 = False;  toggle1 = True
+        elif toggle3:
+            score = re.findall('/cbb/boxscores/.+?>([0-9]+)</a>', linetext)
+            if len(score)==1:
+                toggle3 = False;  toggle1 = True
+                score = int(score[0])
+                if ('&amp;' in school):  school = school.replace('&amp;', '&')
+                if (school in school_dict):  school = school_dict[school]  # rename schools to match stats tables
+
+                cur.execute('SELECT id FROM School WHERE name = ? ', (school, ))
+                try:
+                    school_id = cur.fetchone()
+                except:
+                    print('(!) WARNING:' + school + ' must be added to school_dict')
+                if (school_id==None):  print('(!) WARNING:' + school + ' must be added to school_dict')
+                
+                cur.execute('''INSERT OR IGNORE INTO Rank
+                (season_id, school_id, tourn_rank) 
+                VALUES ( ?, ?, ? )''',
+                (season_id, school_id[0], rank) )
+                if (pair):  # group team scores into pairs to save as game results
+                    school_id2 = school_id[0]
+                    score2 = score
+                    cur.execute('''INSERT OR REPLACE INTO Tournament
+                    (season_id, school_id1, school_id2, score1, score2) 
+                    VALUES ( ?, ?, ?, ?, ? )''',
+                    (season_id, school_id1, school_id2, score1, score2) )
+                    pair = False
+                else:
+                    school_id1 = school_id[0]
+                    score1 = score
+                    pair = True
+                count = count + 1
+            else:
+                toggle3 = False;  toggle1 = True
+    conn.commit()
+    print('   number of games: ' + str(count/2))
+    if (pair): print('      (!) WARNING: not all teams were paired up')
+    else:      print('    - all teams paired')
 
 print('Data scraping successfully completed.')
